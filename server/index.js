d39c87156126b7de55df999112010519e2e64c2d
Nitish Gupta <guptanitish526@gmail.com>
2026-04-13 13:29:46 +0530

    fix module

--- server/index.js
+++ server/index.js
@@ -1,37 +1,21 @@
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import connectDB from "./config/db.js";
+import aiRoutes from "./routes/aiRoutes.js";
 
-const express = require("express");
-const cors = require("cors");
-
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
-const wishlistRoutes = require("./routes/wishlistRoutes");
-const aiRoutes = require("./routes/aiRoutes")
+dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
-app.use("/uploads", express.static("uploads"));
-app.use("/api/auth", authRoutes);
-app.use("/api/product", productRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
-app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/ai", aiRoutes);
 
-// database connect
 connectDB();
 
-// server start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running 🚀");
 });
