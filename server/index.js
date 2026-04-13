54ab1099002fc48211d6c4a14613a7c8a7d50c1a
Nitish Gupta <guptanitish526@gmail.com>
2026-04-13 13:05:48 +0530

    fix port for render

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use("/api/ai", aiRoutes);
 connectDB();
 
 // server start
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
