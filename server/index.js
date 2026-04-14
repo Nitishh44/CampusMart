2f66fd92df279477a431cfd69f349c94d6a879be
Nitish Gupta <guptanitish526@gmail.com>
2026-04-14 11:05:01 +0530

    added auth routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 import aiRoutes from "./routes/aiRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/ai", aiRoutes);
+app.use("/api/auth", authRoutes);
 
 connectDB();
 
