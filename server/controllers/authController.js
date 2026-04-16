c355332a8523e283f44ceea79a9ae82141a5b0a7
Nitish Gupta <guptanitish526@gmail.com>
2026-04-16 18:48:42 +0530

    fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,9 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // Register
-exports.registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
@@ -31,30 +31,26 @@ exports.registerUser = async (req, res) => {
 };
 
 // Login
-exports.loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check user
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
 
-    // Check password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Create token
     const token = jwt.sign(
       { id: user._id },
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
 
-    // Send token + user
     res.status(200).json({
       token,
       user: {
@@ -69,12 +65,13 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-exports.getProfile = async (req, res) => {
+// Profile
+export const getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     res.status(200).json(user);
-  } catch (error){
+  } catch (error) {
     console.log(error);
-    res.status(500).json({message: "Error fetching profile"});
+    res.status(500).json({ message: "Error fetching profile" });
   }
 };
